0e3634dad8fa981f63861e45cc959dacbffa9a44
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2021-06-22 09:56:53 +0530

    Add files via upload

---
+++ WeatherAPI.py
@@ -0,0 +1,30 @@
+import requests
+import sys
+from datetime import datetime
+
+api_key='0153d4ab127d8086b11ca42f21bb5b3a'
+location=input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+
+temperature_city = ((api_data['main']['temp'])-273.15)
+weather_description = api_data['weather'][0]['description']
+humidity = api_data['main']['humidity']
+wind_speed = api_data['wind']['speed']
+date_time=datetime.now().strftime("%d %b %Y | %I:%M")
+
+sys.stdout = open("WeatherAPI.txt","w")
+
+print("___________________________________________________________")
+print("Weather Stats for - {} || {}".format(location.upper(), date_time))
+
+print("Current temperature is   : {:.2f} deg C".format(temperature_city))
+print("Current weather desc     : ",weather_description)
+print("Current Humidity         : ",humidity,'%')
+print("Current Wind Speed       : ",wind_speed,'kmph')
+
+
+sys.stdout.close() 
